cad0947dcbad01745be0f98eccebbf610aba9a4d
youdar <youval@gmail.com>
2013-10-29 09:38:44 -0700

    update

--- work/Clashes/Test_internal_clashscore.py
+++ work/Clashes/Test_internal_clashscore.py
@@ -21,12 +21,24 @@ Output:
 [clashscore_internal,clashscore_probe] : clashscore_internal and clashscore_probe float numbers
 
 >>> python Test_internal_clashscore.py 101m.pdb
-[26,27]
+101m::26.32::27.10
 '''
 
-def clashscore(file_name):
+def clashscore_internal(file_name):
+  '''(str) -> float
+
+  Calculate clashscore using pdb_interpertation code.
+  
+  Argument:
+  file_name: a string, a file name, including the path, of a pdb file
+  to which hydorgen were added
+  
+  Output:
+  clashscore: a float number representning the clashscore of the pdb file
+    
+  '''
   # get the default wroking parameters
-  master_phil = get_master_phil()
+  master_phil = get_master_phil_interpertation()
   input_objects = iotbx.utils.process_command_line_inputs(
     args=file_name,
     master_phil=master_phil,
@@ -47,7 +59,6 @@ def clashscore(file_name):
     substitute_non_crystallographic_unit_cell_if_necessary=True,
     max_atoms=work_params.max_atoms,
     log=sys.stdout)
-    #log=temp_log)
   # conflict handling
   if (work_params.strict_processing):
     msg = processed_pdb_file.all_chain_proxies.fatal_problems_message()
@@ -60,23 +71,15 @@ def clashscore(file_name):
       params_edits=work_params.geometry_restraints.edits,
       params_remove=work_params.geometry_restraints.remove)
     site_cart = processed_pdb_file._geometry_restraints_manager.sites_cart_used_for_pair_proxies()
-    nonboned_proxies = processed_pdb_file._geometry_restraints_manager._clash_proxies
-    clashscore = processed_pdb_file._geometry_restraints_manager._clashscore
+    # Get clashscore
+    return processed_pdb_file._geometry_restraints_manager._clashscore
+  else:
+    # a clashscore of -1 indicates the process did not work
+    return -1
 
-def print_record(x):
-  res1 = x[0][0][5:-1]
-  res2 = x[0][1][5:-1]
-  vdw = x[4]
-  model = x[3]
-  r = model-vdw
-  #  N   ILE A 125
-  [atom1,resName1,chain1,nRes1] = res1.split()
-  [atom2,resName2,chain2,nRes2] = res2.split()
-  res1 = '{0:>2} {1:3}  {2:3} {3:>4}'.format(chain1,nRes1, resName1,atom1)
-  res2 = '{0:>2} {1:3}  {2:3} {3:>4}'.format(chain2,nRes2, resName2,atom2)
-  print '{0}{1} :{2:.3f}'.format(res1,res2,r)
 
-def get_master_phil():
+def get_master_phil_interpertation():
+  # this is a phil for the pdb_interpertation execution
 
   return iotbx.phil.parse("""\
 atom_selection = None
@@ -102,6 +105,17 @@ include scope mmtbx.monomer_library.pdb_interpretation.grand_master_phil_str
 
 
 def get_new_file_name(file_name):
+  '''(str) -> str
+  When using ready_set, a new file will be created, one that includes
+  hydrogens. This function finds the new name and returns it
+  
+  Argument:
+  file_name: a file name of a pdb file, including the path, that was processed 
+  using ready_set
+  
+  Return:
+  file_name: the file name of the modified pdb file, the one that includes hydrogens
+  '''
   file_name = get_pdb_file(file_name)
   cmd  = 'phenix.ready_set {}'.format(file_name)
   probe_out = easy_run.go(cmd)
@@ -110,26 +124,18 @@ def get_new_file_name(file_name):
   file_name = line[0].split('Writing model to ')[-1]
   return file_name
 
-def run2 (args, out=sys.stdout) :
-  '''(str) -> str
+def clashscore_probe(file_name, out=sys.stdout):
+  '''(str) -> float
 
-  This function tests the clash scores of pdb files
-
-  It returns pdb_file_name::score_with_hydrogen::score_without_hydrogen::experment_type
-
-  >>> python test_clashes.py 4iw4
-  4iw4::8.9174::8.9174::X-RAY DIFFRACTION
-
+  Calculate clashscore using probe.
+  
+  Argument:
+  file_name: a string, a file name, including the path, of a pdb file
+  to which hydorgen were added
+  
+  Output:
+  clashscore: a float number representning the clashscore of the pdb file
   '''
-  file_name = args[0]
-  if not os.path.isfile(file_name):
-    osType = sys.platform
-    if osType.startswith('win'):
-      file_name = get_pdb_file(file_name)
-    else:
-      file_name = get_pdb_file_dir(file_name)
-    args[0] = file_name
-  experment_type = get_experment_type(file_name)
   cmdline = iotbx.phil.process_command_line_with_files(
     args=args,
     master_phil=get_master_phil(),
@@ -147,19 +153,11 @@ def run2 (args, out=sys.stdout) :
     nuclear=params.nuclear,
     out=out,
     verbose=False)
-  result_without_hydrogen = mmtbx.validation.clashscore.clashscore(
-      pdb_hierarchy=hierarchy,
-      keep_hydrogens=False,
-      nuclear=params.nuclear,
-      out=out,
-      verbose=False)
-  # build output string
-  file_name  = get_file_name(file_name)
-  score_with_h = result_with_hydrogen.get_clashscore()
-  score_without_h = result_without_hydrogen.get_clashscore()
-  print '{0}::{1:.4f}::{2:.4f}::{3}'.format(file_name,score_with_h,score_without_h,experment_type)
+  return result_with_hydrogen.get_clashscore()
 
-def get_master_phil () :
+
+def get_master_phil() :
+  # phil parameters for the probe clashscore
   return iotbx.phil.parse("""
 model = None
   .type = path
@@ -197,51 +195,19 @@ Example:
 """
 
 
-
-
-
-def get_experment_type(file_name):
-  '''(str) -> str
-  Look for EXPERIMENT TYPE in PDB REMARK
-  REMARK 200 		: X-RAY DIFFRACTION
-  REMARK 210,215,217 	: NMR
-  REMARK 230		: NEUTRON DIFFRACTION
-  REMARK 245		: ELECTRON MICROSCOPE
-  REMARK 250		: Other
-  REMARK 265		: SMALL ANGLE X-RAY SCATTERING
-
-  returns a string combined from all experiment types
-
-  Note that the experiment identification is not done by actually reading the
-  records, but by the record clasification at
-  http://www.wwpdb.org/documentation/format33/remarks1.html
-  '''
-  remark_dict = dict([(200,'X-RAY DIFFRACTION'),
-                      (210,'NMR'),(215,'NMR'),(217,'NMR'),
-                      (230,'NEUTRON DIFFRACTION'),
-                      (245,'ELECTRON MICROSCOPE'),
-                      (250,'Other'),
-                      (265,'SMALL ANGLE X-RAY SCATTERING')])
-  experment_type = []
-  for iii in remark_dict:
-    rem_records = get_remark_iii_records(file_name,iii)
-    if rem_records != []:
-      experment_type.append(remark_dict[iii])
-
-  return '::'.join(experment_type)
-
-def get_remark_iii_records(file_name,iii):
-  pdb_inp = pdb.hierarchy.input(file_name=file_name)
-  pdb_inp_tls = tls_from_pdb_inp(
-    remark_3_records = pdb_inp.input.extract_remark_iii_records(iii),
-    pdb_hierarchy = pdb_inp.hierarchy)
-  return pdb_inp_tls.remark_3_records
-
 if (__name__ == "__main__"):
   set_working_path('Clashes\junk')
   file_name = sys.argv[1]
   # get the file name of the file that includes hydrogens
   file_name = get_new_file_name(file_name)
+  #
+  print 'Starting'
+  print os.getcwd()
+  print file_name
+  print '*'*80
 
-
-  run([file_name])
+  clashscore_internal = clashscore_internal(file_name)
+  clashscore_probe = clashscore_probe(file_name, out=sys.stdout)
+  
+  print '*'*80
+  print 'done'

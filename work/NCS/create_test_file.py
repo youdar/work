3b097ed2c9c030829b74bc059a2d8ae3cff4c18e
youdar <youval@gmail.com>
2014-01-16 16:32:26 -0800

    update

---
+++ work/NCS/create_test_file.py
@@ -0,0 +1,102 @@
+from __future__ import division
+from iotbx.pdb.multimer_reconstruction import multimer
+from mmtbx import utils
+from iotbx import reflection_file_reader
+from libtbx import easy_run
+from iotbx import pdb
+import unittest
+import cProfile
+import shutil
+import zipfile
+import tempfile
+from cctbx import crystal
+import sys,os
+
+'''
+Create test file for testing strict Non-crystallographic symmetry (NCS)
+application in refinement
+
+@author Youval Dar (LBL 2014)
+'''
+
+def run():
+  '''
+  create a temporary folder with files for testing.
+  Using ncs0_pdb, which contains a single NCS, CRYST1 and MTRIX records,
+  produce:
+  1) ncs0.pdb, with CRYST1 and SCALE records
+  2) asu0.pdb, with complete Asymmetric Unit (ASU)
+  3) ncs1.pdb  pertubed version of ncs0_pdb, with a single NCS and MTRIX info
+  '''
+  currnet_dir = os.getcwd()
+  #tempdir = empfile.mkdtemp('tempdir')
+  #os.chdir(tempdir)
+  #
+  testdir = r'C:\Phenix\Dev\Work\work\NCS\test_files'
+  os.chdir(testdir)
+  #
+  print 'working at {}'.format(os.getcwd())
+  #
+  asu0_filename = 'asu0.pdb'
+  ncs1_filename = 'ncs1.pdb'
+  ncs0_filename = 'ncs0.pdb'
+  #
+  f = open(ncs0_filename,'w')
+  f.write(ncs0_pdb)
+  f.close()
+  # Create the ASU coordinates using MTRIX records
+  # Do it before creating the CRYST1 records, when creating them the MTRIX will not be saved
+  m = multimer(ncs0_filename,'cau',error_handle=True,eps=1e-2)
+  if m.number_of_transforms == 0:
+    print 'Number of transforms is zero'
+  m.write(asu0_filename)
+  pdb_inp = pdb.input(file_name = asu0_filename)
+  xrs = pdb_inp.xray_structure_simple()
+  crystal_symmetry = xrs.crystal_symmetry()
+  pdb_inp.write_pdb_file(file_name = asu0_filename, crystal_symmetry = crystal_symmetry)
+  pdb_inp.crystal_symmetry()
+  print '*'*100
+  print 'Complete Asymmetric Unit (ASU) - use as target'
+  get_file_as_str(asu0_filename)
+  # Create and write a file ncs0.pdb
+  pdb_inp = pdb.input(file_name = ncs0_filename)
+  xrs = pdb_inp.xray_structure_simple()
+  crystal_symmetry = xrs.crystal_symmetry()
+  pdb_inp.write_pdb_file(file_name = ncs0_filename, crystal_symmetry = crystal_symmetry)
+  print '*'*100
+  print 'For the NCS remember to add the MTRIX portion (see below) back to the pdb'
+  get_file_as_str(ncs0_filename)
+  print 'The MTRIX records for the NCS'
+  print ncs0_pdb
+  print 'Done.'
+
+def get_file_as_str(fn):
+  print '='*100
+  print open(fn,'r').read()
+  print '='*100
+
+
+
+# Raw data used to build test cases
+ncs0_pdb="""\
+MTRIX1   1  1.000000  0.000000  0.000000        0.00000    1
+MTRIX2   1  0.000000  1.000000  0.000000        0.00000    1
+MTRIX3   1  0.000000  0.000000  1.000000        0.00000    1
+MTRIX1   2  0.496590 -0.643597  0.582393        0.00000
+MTRIX2   2  0.867925  0.376088 -0.324443        0.00000
+MTRIX3   2 -0.010221  0.666588  0.745356        0.00000
+MTRIX1   3 -0.317946 -0.173437  0.932111        0.00000
+MTRIX2   3  0.760735 -0.633422  0.141629        0.00000
+MTRIX3   3  0.565855  0.754120  0.333333        0.00000
+ATOM      1  N   THR 1   1       9.483   9.256  10.995  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.489   7.820  10.603  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.725   6.911  11.796  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.586   7.189  12.629  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.632   7.506   9.642  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.831   8.604   8.746  1.00 67.35           O
+ATOM      7  CG2 THR 1   1      10.308   6.254   8.859  1.00 43.17           C
+TER
+"""
+
+if __name__ == "__main__":
+  run()
--- work/NCS/test_strict_NCS.py
+++ work/NCS/test_strict_NCS.py
@@ -1,4 +1,6 @@
 from __future__ import division
+from iotbx import reflection_file_reader
+from libtbx import easy_run
 from iotbx import pdb
 import unittest
 import cProfile
@@ -31,11 +33,49 @@ class TestStrictNCS(unittest.TestCase):
     2) ncs1.pdb  pertubed version of ncs0_pdb, with a single NCS and MTRIX info
     '''
     self.currnet_dir = os.getcwd()
-    self.testdir = tempfile.mkdtemp('testdir')
-    os.chdir(self.testdir)
+    self.tempdir = tempfile.mkdtemp('tempdir')
+    os.chdir(self.tempdir)
     self.asu0_filename = 'asu0.pdb'
     self.ncs1_filename = 'ncs1.pdb'
     # Create and write a file ncs0.pdb
+    prefix = 'ncs0'
+    pdb_inp = pdb.input(source_info=None, lines=ncs0_pdb)
+    pdb_inp.write_pdb_file(file_name = '{}.pdb'.format(prefix))
+    xrs = pdb_inp.xray_structure_simple()
+    f_obs = abs(xrs.structure_factors(d_min = 2).f_calc())
+    mtz_dataset = f_obs.as_mtz_dataset(column_root_label="F-obs")
+    mtz_dataset.add_miller_array(
+      miller_array=f_obs.generate_r_free_flags(),
+      column_root_label="R-free-flags")
+    mtz_object = mtz_dataset.mtz_object()
+    mtz_object.write(file_name = '{}_map.mtz'.format(prefix))
+    # Process the mtz_object
+    miller_arrays = mtz_object.as_miller_arrays()
+    #
+    #cmd = " ".join([
+      #"phenix.refine",
+      #"{0}.pdb {0}_map.mtz".format(prefix),
+      #"strategy=none",
+      #"main.number_of_macro_cycles=0",
+      #"output.prefix={}".format(prefix),
+      #"--overwrite",
+      #"--quiet"])
+    #easy_run.call(cmd)
+    #miller_arrays = reflection_file_reader.any_reflection_file(
+      #file_name = "{}_map.mtz".format(prefix)).as_miller_arrays()
+    labels = []
+    for ma in miller_arrays:
+      l = ",".join(ma.info().labels)
+      if(ma.is_complex_array()): labels.append(l)
+    for i, l in enumerate(labels):
+      cmd = " ".join([
+        "phenix.map_box", pdbf, "%s_map.mtz"%prefix,
+        "label='%s'"%l,
+        ">zlog_%s_%s"%(str(i),prefix)])
+      easy_run.call(cmd)
+
+    print os.getcwd()
+
 
     # Create and write asu0.pdb
     f = open(self.asu0_filename,'w')
@@ -68,25 +108,62 @@ class TestStrictNCS(unittest.TestCase):
 
   #def test_2(self):
     #'''Test that Error is raised when trying to archive the current directory'''
-    ##self.assertRaises(ValueError,archive_dir.archive_dir_files,self.testdir)
+    ##self.assertRaises(ValueError,archive_dir.archive_dir_files,self.tempdir)
     #pass
 
 
   def tearDown(self):
     '''remove temp files and folder'''
     os.chdir(self.currnet_dir)
-    shutil.rmtree(self.testdir)
+    shutil.rmtree(self.tempdir)
 
 
 # Raw data used to build test cases
 ncs0_pdb="""\n
-CRYST1   20.000   20.000   20.000  90.00  90.00  90.00 P 1
-ATOM      1  N   LYS     1       5.000   5.000   5.000  1.00 20.00           N
-ATOM      1  N   LYS     2       6.000   5.000   5.000  1.00 20.00           N
-ATOM      1  N   LYS     4       5.000   5.500   5.500  1.00 20.00           N
+CRYST1    9.114   10.768   11.649  90.00  90.00  90.00 P 1
+SCALE1      0.109721  0.000000  0.000000        0.00000
+SCALE2      0.000000  0.092868  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.085844        0.00000
+ATOM      1  N   THR 1   1       9.483   9.256  10.995  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.489   7.820  10.603  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.725   6.911  11.796  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.586   7.189  12.629  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.632   7.506   9.642  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.831   8.604   8.746  1.00 67.35           O
+ATOM      7  CG2 THR 1   1      10.308   6.254   8.859  1.00 43.17           C
 TER
-END
 """
+asu0_pdb="""\n
+CRYST1   22.845   22.121   22.495  90.00  90.00  90.00 P 1
+SCALE1      0.043773  0.000000  0.000000        0.00000
+SCALE2      0.000000  0.045206  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.044454        0.00000
+ATOM      1  N   THR 1   1       9.483   9.256  10.995  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.489   7.820  10.603  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.725   6.911  11.796  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.586   7.189  12.629  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.632   7.506   9.642  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.831   8.604   8.746  1.00 67.35           O
+ATOM      7  CG2 THR 1   1      10.308   6.254   8.859  1.00 43.17           C
+TER
+ATOM      1  N   THRaa   1       5.155   8.144  14.268  1.00 26.11           N
+ATOM      2  CA  THRaa   1       5.854   7.737  13.019  1.00 27.16           C
+ATOM      3  C   THRaa   1       7.251   7.213  13.300  1.00 20.29           C
+ATOM      4  O   THRaa   1       7.985   7.794  14.097  1.00 35.00           O
+ATOM      5  CB  THRaa   1       6.064   8.922  12.081  1.00 34.84           C
+ATOM      6  OG1 THRaa   1       4.935   9.799  12.144  1.00 67.35           O
+ATOM      7  CG2 THRaa   1       6.253   8.424  10.667  1.00 43.17           C
+TER
+ATOM      1  N   THRab   1       5.628   2.908  16.011  1.00 26.11           N
+ATOM      2  CA  THRab   1       5.510   3.767  14.801  1.00 27.16           C
+ATOM      3  C   THRab   1       6.705   4.691  14.647  1.00 20.29           C
+ATOM      4  O   THRab   1       7.159   5.288  15.621  1.00 35.00           O
+ATOM      5  CB  THRab   1       4.305   4.699  14.891  1.00 34.84           C
+ATOM      6  OG1 THRab   1       3.216   4.028  15.533  1.00 67.35           O
+ATOM      7  CG2 THRab   1       3.896   5.135  13.502  1.00 43.17           C
+TER
+"""
+
 
 if __name__ == "__main__":
   unittest.main(verbosity=2)  # provides a command-line interface to the test script

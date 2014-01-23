e0af77747d23829bfb01ea0ad58246a3ec2c4ed5
youdar <youval@gmail.com>
2014-01-22 17:02:38 -0800

    cleanup

--- work/NCS/test_strict_NCS.py
+++ work/NCS/test_strict_NCS.py
@@ -42,9 +42,11 @@ class TestStrictNCS(unittest.TestCase):
     self.currnet_dir = os.getcwd()
     self.tempdir = tempfile.mkdtemp('tempdir')
     # for testing use junk folder insted of real temp directory
-    os.chdir('/net/cci/youval/Work/work/NCS/junk')
-    #os.mkdir('junk')
-    self.tempdir = '/net/cci/youval/Work/work/NCS/junk'
+    osType = sys.platform
+    if osType.startswith('win'):
+      self.tempdir = (r'C:\Phenix\Dev\Work\work\NCS\junk')
+    else:
+      self.tempdir = ('/net/cci/youval/Work/work/NCS/junk')
     # remember to change back the clean up when going back to real temp dir
     #
     os.chdir(self.tempdir)
@@ -68,9 +70,9 @@ class TestStrictNCS(unittest.TestCase):
   def test_process_integrity(self):
     ''' Test that when comparing asu0 to itself we get R-work is zero'''
     # Reconstruct ncs0 and retrive f_obs
-    m = self.build_asu(file_name_ncs=self.ncs0_filename, file_name_asu='asu0_calc.pdb')
-    f_obs_calc = self.get_f_calc(file_name='asu0_calc.pdb')
-    r_factor = self.calc_r_factor(self.f_obs_asu0,f_obs_calc)
+    m = self.build_asu(file_name_ncs=self.ncs0_filename, file_name_asu='asu0.pdb')
+    f_calc = self.get_f_calc(file_name='asu0.pdb')
+    r_factor = self.calc_r_factor(self.f_obs_asu0,f_calc)
     msg='Problem with test data, f_obs from ASU do not match those from from the same ASU as constructed by NCS'
     self.assertEqual(r_factor,0, msg)
 
@@ -79,9 +81,9 @@ class TestStrictNCS(unittest.TestCase):
     by checking that R-work is not zero
     Compare f_obs from asu0.pdb to f_calc from asu1.pdb'''
     # Reconstruct ncs1 and retrive f_obs
-    m = self.build_asu(file_name_ncs=self.ncs1_filename, file_name_asu='asu1_calc.pdb')
-    f_obs_calc = self.get_f_calc(file_name='asu1_calc.pdb')
-    r_factor = self.calc_r_factor(self.f_obs_asu0,f_obs_calc)
+    m = self.build_asu(file_name_ncs=self.ncs1_filename, file_name_asu='asu1.pdb')
+    f_calc = self.get_f_calc(file_name='asu1.pdb')
+    r_factor = self.calc_r_factor(self.f_obs_asu0,f_calc)
     msg='''\
     Problem with test data, f_obs from ASU do not match those from
     the same ASU as constructed by NCS'''
@@ -96,11 +98,9 @@ class TestStrictNCS(unittest.TestCase):
     m_asu1 = self.build_asu(file_name_ncs=self.ncs1_filename, file_name_asu=self.asu1_filename)
     #
     f_obs = abs(self.get_f_calc(file_name=self.asu0_filename))
-    f_calc_asu1 = self.get_f_calc(file_name=self.asu1_filename)
+    f_calc = self.get_f_calc(file_name=self.asu1_filename)
     # r_factor at start
-    r_factor = self.calc_r_factor(f_obs,f_calc_asu1)
-    # Make sure the difference between the f_calc, at start, is segnificant
-    self.assertTrue(r_factor > 0.05)
+    r_factor = self.calc_r_factor(f_obs,f_calc)
     # Refine
     # 1) create mtz file
     file_name_mtz = self.asu0_filename.split('.')[0] + '_map.mtz'
@@ -113,22 +113,19 @@ class TestStrictNCS(unittest.TestCase):
       #pdb_file_symmetry_target=self.asu0_filename)
     # Process refinement resaults
     file_name_refined = 'refine_output_001.pdb'
+    self.assertTrue(os.path.isfile(file_name_refined),'No {} refined pdb file'.format(file_name_refined))
     f_calc = self.get_f_calc(file_name=file_name_refined)
     r_factor_refined = self.get_r_factor(f_obs=f_obs,f_calc=f_calc)
-    print r_factor
-    print r_factor_refined
-    print 'done with refinement'
-
-
-
-
+    msg1='r_factor is {0:.5f}. very small before refinement.'.format(r_factor)
+    self.assertTrue(r_factor>0.3,msg1)
+    msg2='Refinement did not work well, r_factor before {0:.5f}  after {1:.5f}'.format(r_factor,r_factor_refined)
+    self.assertTrue(r_factor_refined<0.05,msg2)
 
 
   #def test_2(self):
     #'''Test that Error is raised when trying to archive the current directory'''
     ##self.assertRaises(ValueError,archive_dir.archive_dir_files,self.tempdir)
     #pass
-
 
   def tearDown(self):
     '''remove temp files and folder'''
@@ -156,16 +153,14 @@ class TestStrictNCS(unittest.TestCase):
     cmd = " ".join([
       "phenix.refine",
       "{0} {1}".format(pdb_file,mtz_file),
-      #"strategy=none",
+      "strategy=individual_sites",
       "main.number_of_macro_cycles=3",
       "output.prefix={}".format(output_file_name),
       "--overwrite",
       "--quiet"])
     if pdb_file_symmetry_target:
       cmd = ' '.join([cmd,"--symmetry={}".format(pdb_file_symmetry_target)])
-    print cmd
     easy_run.call(cmd)
-
 
   def make_mtz_file(self,f_obs,file_name_mtz):
     '''
@@ -237,10 +232,10 @@ class TestStrictNCS(unittest.TestCase):
 
 # Raw data used to build test cases
 ncs0_pdb="""\
-CRYST1   23.826   24.019   24.057  90.00  90.00  90.00 P 1
-SCALE1      0.041971  0.000000  0.000000        0.00000
-SCALE2      0.000000  0.041634  0.000000        0.00000
-SCALE3      0.000000  0.000000  0.041568        0.00000
+CRYST1   24.667   24.779   25.359  90.00  90.00  90.00 P 1
+SCALE1      0.040540  0.000000  0.000000        0.00000
+SCALE2      0.000000  0.040357  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.039434        0.00000
 MTRIX1   1  1.000000  0.000000  0.000000        0.00000    1
 MTRIX2   1  0.000000  1.000000  0.000000        0.00000    1
 MTRIX3   1  0.000000  0.000000  1.000000        0.00000    1
@@ -250,50 +245,51 @@ MTRIX3   2 -0.010221  0.666588  0.745356        0.00000
 MTRIX1   3 -0.317946 -0.173437  0.932111        0.00000
 MTRIX2   3  0.760735 -0.633422  0.141629        0.00000
 MTRIX3   3  0.565855  0.754120  0.333333        0.00000
-ATOM      1  N   THR 1   1       9.483  10.256  10.995  1.00 26.11           N
-ATOM      2  CA  THR 1   1       9.489   8.820  10.603  1.00 27.16           C
-ATOM      3  C   THR 1   1       9.725   7.911  11.796  1.00 20.29           C
-ATOM      4  O   THR 1   1      10.586   8.189  12.629  1.00 35.00           O
-ATOM      5  CB  THR 1   1      10.632   8.506   9.642  1.00 34.84           C
-ATOM      6  OG1 THR 1   1      10.831   9.604   8.746  1.00 67.35           O
-ATOM      7  CG2 THR 1   1      10.308   7.254   8.859  1.00 43.17           C
+ATOM      1  N   THR 1   1       9.670  10.289  11.135  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.559   8.931  10.615  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.634   7.903  11.739  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.449   8.027  12.653  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.660   8.630   9.582  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.560   9.552   8.490  1.00 67.35           O
+ATOM      7  CG2 THR 1   1      10.523   7.209   9.055  1.00 43.17           C
+TER
 """
 asu0_pdb="""\
-CRYST1   23.826   24.019   24.057  90.00  90.00  90.00 P 1
-SCALE1      0.041971  0.000000  0.000000        0.00000
-SCALE2      0.000000  0.041634  0.000000        0.00000
-SCALE3      0.000000  0.000000  0.041568        0.00000
-ATOM      1  N   THR 1   1       9.483  10.256  10.995  1.00 26.11           N
-ATOM      2  CA  THR 1   1       9.489   8.820  10.603  1.00 27.16           C
-ATOM      3  C   THR 1   1       9.725   7.911  11.796  1.00 20.29           C
-ATOM      4  O   THR 1   1      10.586   8.189  12.629  1.00 35.00           O
-ATOM      5  CB  THR 1   1      10.632   8.506   9.642  1.00 34.84           C
-ATOM      6  OG1 THR 1   1      10.831   9.604   8.746  1.00 67.35           O
-ATOM      7  CG2 THR 1   1      10.308   7.254   8.859  1.00 43.17           C
+CRYST1   24.667   24.779   25.359  90.00  90.00  90.00 P 1
+SCALE1      0.040540  0.000000  0.000000        0.00000
+SCALE2      0.000000  0.040357  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.039434        0.00000
+ATOM      1  N   THR 1   1       9.670  10.289  11.135  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.559   8.931  10.615  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.634   7.903  11.739  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.449   8.027  12.653  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.660   8.630   9.582  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.560   9.552   8.490  1.00 67.35           O
+ATOM      7  CG2 THR 1   1      10.523   7.209   9.055  1.00 43.17           C
 TER
-ATOM      1  N   THRaa   1       4.512   8.520  14.935  1.00 26.11           N
-ATOM      2  CA  THRaa   1       5.211   8.113  13.685  1.00 27.16           C
-ATOM      3  C   THRaa   1       6.608   7.589  13.966  1.00 20.29           C
-ATOM      4  O   THRaa   1       7.342   8.170  14.764  1.00 35.00           O
-ATOM      5  CB  THRaa   1       5.421   9.299  12.748  1.00 34.84           C
-ATOM      6  OG1 THRaa   1       4.291  10.175  12.810  1.00 67.35           O
-ATOM      7  CG2 THRaa   1       5.610   8.800  11.333  1.00 43.17           C
+ATOM      1  N   THRaa   1       4.665   8.650  15.059  1.00 26.11           N
+ATOM      2  CA  THRaa   1       5.181   8.211  13.768  1.00 27.16           C
+ATOM      3  C   THRaa   1       6.535   7.525  13.919  1.00 20.29           C
+ATOM      4  O   THRaa   1       7.392   7.983  14.675  1.00 35.00           O
+ATOM      5  CB  THRaa   1       5.320   9.389  12.786  1.00 34.84           C
+ATOM      6  OG1 THRaa   1       4.041  10.003  12.587  1.00 67.35           O
+ATOM      7  CG2 THRaa   1       5.859   8.907  11.447  1.00 43.17           C
 TER
-ATOM      1  N   THRab   1       5.455   2.275  16.765  1.00 26.11           N
-ATOM      2  CA  THRab   1       5.336   3.134  15.555  1.00 27.16           C
-ATOM      3  C   THRab   1       6.531   4.058  15.401  1.00 20.29           C
-ATOM      4  O   THRab   1       6.986   4.655  16.375  1.00 35.00           O
-ATOM      5  CB  THRab   1       4.132   4.066  15.645  1.00 34.84           C
-ATOM      6  OG1 THRab   1       3.043   3.395  16.287  1.00 67.35           O
-ATOM      7  CG2 THRab   1       3.722   4.502  14.256  1.00 43.17           C
+ATOM      1  N   THRab   1       5.520   2.416  16.943  1.00 26.11           N
+ATOM      2  CA  THRab   1       5.306   3.118  15.682  1.00 27.16           C
+ATOM      3  C   THRab   1       6.508   3.986  15.324  1.00 20.29           C
+ATOM      4  O   THRab   1       7.080   4.656  16.184  1.00 35.00           O
+ATOM      5  CB  THRab   1       4.045   4.000  15.734  1.00 34.84           C
+ATOM      6  OG1 THRab   1       2.899   3.185  16.009  1.00 67.35           O
+ATOM      7  CG2 THRab   1       3.844   4.721  14.409  1.00 43.17           C
 TER
 """
 
 ncs1_pdb = """\
-CRYST1   23.826   24.019   24.057  90.00  90.00  90.00 P 1
-SCALE1      0.041971  0.000000  0.000000        0.00000
-SCALE2      0.000000  0.041634  0.000000        0.00000
-SCALE3      0.000000  0.000000  0.041568        0.00000
+CRYST1   24.667   24.779   25.359  90.00  90.00  90.00 P 1
+SCALE1      0.040540  0.000000  0.000000        0.00000
+SCALE2      0.000000  0.040357  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.039434        0.00000
 MTRIX1   1  1.000000  0.000000  0.000000        0.00000    1
 MTRIX2   1  0.000000  1.000000  0.000000        0.00000    1
 MTRIX3   1  0.000000  0.000000  1.000000        0.00000    1
@@ -303,13 +299,13 @@ MTRIX3   2 -0.010221  0.666588  0.745356        0.00000
 MTRIX1   3 -0.317946 -0.173437  0.932111        0.00000
 MTRIX2   3  0.760735 -0.633422  0.141629        0.00000
 MTRIX3   3  0.565855  0.754120  0.333333        0.00000
-ATOM      1  N   THR 1   1       9.514  10.278  10.935  1.00 26.11           N
-ATOM      2  CA  THR 1   1       9.519   8.854  10.711  1.00 27.16           C
-ATOM      3  C   THR 1   1       9.699   7.867  11.829  1.00 20.29           C
-ATOM      4  O   THR 1   1      10.692   8.121  12.544  1.00 35.00           O
-ATOM      5  CB  THR 1   1      10.676   8.475   9.646  1.00 34.84           C
-ATOM      6  OG1 THR 1   1      10.859   9.626   8.815  1.00 67.35           O
-ATOM      7  CG2 THR 1   1      10.262   7.276   8.864  1.00 43.17           C
+ATOM      1  N   THR 1   1      10.904  10.178  11.494  1.00 26.11           N
+ATOM      2  CA  THR 1   1       9.292   8.774  11.496  1.00 27.16           C
+ATOM      3  C   THR 1   1       9.382   8.627  11.583  1.00 20.29           C
+ATOM      4  O   THR 1   1      10.030   8.190  12.474  1.00 35.00           O
+ATOM      5  CB  THR 1   1      10.864   8.019   9.596  1.00 34.84           C
+ATOM      6  OG1 THR 1   1      10.347   9.523   7.536  1.00 67.35           O
+ATOM      7  CG2 THR 1   1       9.170   6.827   9.870  1.00 43.17           C
 TER
 """
 
